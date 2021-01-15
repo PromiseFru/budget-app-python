cc0d8f2eeec34e95a3411e340a7af904175e15d4
Promise Fru <promisefru1@gmail.com>
2021-01-15 13:43:10 +0100

    ledger instance var created, deposit method created

--- budget.py
+++ budget.py
@@ -1,7 +1,24 @@
 class Category:
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
+
+    def deposit(self, amount, description = ""):
+        self.ledger.append({
+            "amount": amount,
+            "description": description
+        })
 
 
+# food = Category("food")
+# food.deposite(900,"food")
 
+# cloth = Category("cloth")
+# cloth.deposite(500)
+
+# print(food.ledger[0])
+# print(cloth.ledger[0])
 
 
 def create_spend_chart(categories):
+    return "hello world"
