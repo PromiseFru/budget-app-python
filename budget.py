63a817de40842e22f5e17379304fa74613c9d706
Promise Fru <promisefru1@gmail.com>
2021-01-17 23:14:28 +0100

    transfer method in progress

--- budget.py
+++ budget.py
@@ -19,21 +19,23 @@ class Category:
         balance = 0
         for i in self.ledger:
             balance += i['amount']
-        return balance    
-    
+        return balance
+
+    def transfer(self, amount, category):
+        self.withdraw(amount, "Transfer to " + category)    
 
 
-# food = Category("food")
-# food.deposit(900,"food")
+food = Category("food")
+food.transfer(900,"cloth")
 # food.withdraw(100,"food2")
 # food.deposit(50,"food3")
 
 
-# cloth = Category("cloth")
+cloth = Category("cloth")
 # cloth.deposite(500)
 
 # print(food.get_balance())
-# print(cloth.ledger[0])
+print(cloth.ledger)
 
 
 def create_spend_chart(categories):
